f00c1553fc248719734cd0b7d6891823d589856f
e <1367592252@qq.com>
2019-12-11 23:10:34 +0800

    [es] java客户端操作

--- src/main/java/com/example/es/api/ESAPi.java
+++ src/main/java/com/example/es/api/ESAPi.java
@@ -1,41 +1,48 @@
 package com.example.es.api;
 
 import com.example.es.bean.ESIndexSettingAndMapping;
-import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.ActionFuture;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
-import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.IndicesAdminClient;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.SearchHits;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 
 @Component
@@ -44,35 +51,70 @@ public class ESAPi {
     @Autowired
     TransportClient client;
 
-    // 创建文档
-    public boolean createIndexAndMapping(String indexName, ESIndexSettingAndMapping mapping) throws ExecutionException, InterruptedException, IOException {
+    // cerateIndex
+    public boolean createIndex(String indexName, ESIndexSettingAndMapping mapping) {
         if (!indexExists(indexName)) {
             CreateIndexRequest request = new CreateIndexRequest(indexName);
-            CreateIndexResponse createIndexResponse = client.admin().indices().create(request).actionGet();
+            CreateIndexResponse createIndexResponse = client.admin().indices()
+                    .prepareCreate(indexName)
+                    .setSettings(
+                            Settings.builder()
+                                    .put("index.number_of_shards", mapping.getSetting().getNumber_of_shards())
+                                    .put("index.number_of_replicas", mapping.getSetting().getNumber_of_replicas())
+                    ).get();
+            return createIndexResponse.isAcknowledged();
+        }
+        return false;
+    }
+
+    // 创建文档
+    public boolean putMapping(String indexName, ESIndexSettingAndMapping mapping) throws ExecutionException, InterruptedException, IOException {
+        if (indexExists(indexName)) {
+            CreateIndexRequest request = new CreateIndexRequest(indexName);
+            CreateIndexRequestBuilder indexRequestBuilder = client.admin().indices()
+                    .prepareCreate(indexName);
 
             //创建mapping约束字段
-            XContentBuilder xContentMapping = XContentFactory.jsonBuilder().startObject()
-                    .startObject("properties")
-                    .startObject("title")
-                    .field("type","text")
-                    .endObject()
-                    .startObject("publishDate")
-                    .field("type", "date")
-                    .endObject()
-                    .startObject("content")
-                    .field("type","text")
-                    .endObject()
-                    .startObject("director")
-                    .field("type", "keyword")
-                    .endObject()
-                    .startObject("price")
-                    .field("type", "float")
-                    .endObject()
-                    .endObject()
-                    .endObject();
+            XContentBuilder xContentMappingBuilder = jsonBuilder()
+                    .startObject() //{
+                    .startObject(mapping.getMappings().getDocumentName()) // type
+                    .startObject("properties"); // properties
+
+            for (ESIndexSettingAndMapping.Mappings.Property p : mapping.getMappings().getProperties()) {
+
+                if (StringUtils.isEmpty(p.getProperName()) || StringUtils.isEmpty(p.getProperType())) {
+                    continue;
+                } else {
+                    xContentMappingBuilder.startObject(p.getProperName());
+
+                    // 属性
+                    xContentMappingBuilder.field("type", p.getProperType());
+
+                    if (StringUtils.isEmpty(p.getStore())) { //store
+                        xContentMappingBuilder.field("store", p.getStore());
+                    }
+                    if (StringUtils.isEmpty(p.getAnalyzer())) { //store
+                        xContentMappingBuilder.field("analyzer", p.getAnalyzer());
+                    }
+                    if (StringUtils.isEmpty(p.getStore())) { //store
+                        xContentMappingBuilder.field("index", p.getIndex());
+                    }
+                    if (StringUtils.isEmpty(p.getStore())) { //store
+                        xContentMappingBuilder.field("format", p.getFormat());
+                    }
+                    xContentMappingBuilder.endObject();
+                }
+            }
+
+            xContentMappingBuilder.endObject(); //properties
+            xContentMappingBuilder.endObject(); // typeName
+            xContentMappingBuilder.endObject(); // }
+
             //添加mapping 绑定到 index
-            PutMappingRequest putMappingRequest = Requests.putMappingRequest("film").type("dongzuo").source(mapping);
-            client.admin().indices().putMapping(putMappingRequest).actionGet();
+            CreateIndexResponse response = indexRequestBuilder
+                    .addMapping(mapping.getMappings().getDocumentName(), xContentMappingBuilder)
+                    .get();
+            return response.isAcknowledged();
         }
         return false;
     }
@@ -91,5 +133,88 @@ public class ESAPi {
         DeleteIndexRequest request = new DeleteIndexRequest(indexName);
         ActionFuture<DeleteIndexResponse> delete = client.admin().indices().delete(request);
         return delete.actionGet().isAcknowledged();
+    }
+
+    // 添加文档数据
+    public boolean addDocument(String indexName, String documentName, HashMap<String, Object> jsonMap) {
+        IndexResponse indexResponse = client.prepareIndex(indexName, documentName).setSource(jsonMap).get();
+        return indexResponse.status() == RestStatus.OK;
+    }
+
+    // 更新文档数据
+    public boolean updateDocument(String indexName, String documentName, String id, HashMap<String, Object> jsonMap) {
+        UpdateRequest updateRequest = new UpdateRequest(indexName, documentName, id).doc(jsonMap);
+        UpdateResponse updateResponse = null;
+        try {
+            updateResponse = client.update(updateRequest).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return updateResponse.getGetResult().isExists();
+    }
+
+    // 删除数据
+    public void deleteDocument(String indexName, String documentName, String id) {
+        DeleteRequest deleteRequest = new DeleteRequest(indexName, documentName, id);
+        try {
+            client.delete(deleteRequest).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 通过id查找
+    public Map<String, Object> getResById(String index, String document, String id) {
+        GetResponse response = client.prepareGet(index, document, id).get();
+        return response.getSource();
+    }
+
+    // 查找
+    public List<Map<String, Object>> search(String index, String document, String property, String key, Integer start, Integer size) {
+        final List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        SearchResponse searchResponse = client.prepareSearch().setTypes(index, document)
+                .setSearchType(SearchType.QUERY_THEN_FETCH)
+                .setQuery(QueryBuilders.matchQuery(property, key))
+                .setFrom(start)
+                .setSize(size)
+                .setExplain(true)
+                .get();
+        long totalHits1 = searchResponse.getHits().totalHits;  //命中个数
+        for (SearchHit searchHit : searchResponse.getHits().getHits()) {
+            final Map<String, Object> sourceAsMap = searchHit.getSourceAsMap();
+            list.add(sourceAsMap);
+        }
+        return list;
+    }
+
+    // 批量增加
+    public void addDocuments(String indexName, String document, HashMap<String, HashMap<String, Object>> map) throws IOException {
+        // 创建批量请求构造器
+        BulkRequestBuilder bulkRequest = client.prepareBulk();
+
+        // 遍历数据
+        for (String key : map.keySet()) {
+
+            // 其中一条数据
+            HashMap<String, Object> doc = map.get(key);
+            XContentBuilder xContentBuilder = jsonBuilder().startObject();
+
+            for (String p : doc.keySet()) {
+                // 注入属性 值
+                xContentBuilder.field(p, doc.get(p));
+            }
+            xContentBuilder.endObject();
+
+            // indexName 索引, document 文档, key id
+            IndexRequestBuilder builder = client.prepareIndex(indexName, document, key)
+                    .setSource(xContentBuilder);
+            // 添加到 构造器里面
+            bulkRequest.add(builder);
+        }
+        bulkRequest.get();
     }
 }
--- src/main/java/com/example/es/bean/ESIndexSettingAndMapping.java
+++ src/main/java/com/example/es/bean/ESIndexSettingAndMapping.java
@@ -2,7 +2,6 @@ package com.example.es.bean;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.omg.CORBA.PRIVATE_MEMBER;
 
 import java.util.List;
 
@@ -13,19 +12,67 @@ public class ESIndexSettingAndMapping {
 
     private Mappings mappings;
 
+    public Setting getSetting() {
+        return setting;
+    }
+
+    public void setSetting(Setting setting) {
+        this.setting = setting;
+    }
+
+    public Mappings getMappings() {
+        return mappings;
+    }
+
+    public void setMappings(Mappings mappings) {
+        this.mappings = mappings;
+    }
+
     @Data
     @AllArgsConstructor
     public static class Setting {
         Integer number_of_shards;
         Integer number_of_replicas;
+
+        public Integer getNumber_of_shards() {
+            return number_of_shards;
+        }
+
+        public void setNumber_of_shards(Integer number_of_shards) {
+            this.number_of_shards = number_of_shards;
+        }
+
+        public Integer getNumber_of_replicas() {
+            return number_of_replicas;
+        }
+
+        public void setNumber_of_replicas(Integer number_of_replicas) {
+            this.number_of_replicas = number_of_replicas;
+        }
     }
 
     @Data
     public static class Mappings {
 
-        private String typeName;
+        private String documentName;
 
         private List<Property> properties;
+
+        public String getDocumentName() {
+            return documentName;
+        }
+
+        public void setDocumentName(String documentName) {
+            this.documentName = documentName;
+        }
+
+        public List<Property> getProperties() {
+            return properties;
+        }
+
+        public void setProperties(List<Property> properties) {
+            this.properties = properties;
+        }
 
         @Data
         public static class Property {
@@ -33,6 +80,56 @@ public class ESIndexSettingAndMapping {
             private String properType;
             private String analyzer;
             private String format;
+            private String store;
+            private String index;
+
+            public String getIndex() {
+                return index;
+            }
+
+            public void setIndex(String index) {
+                this.index = index;
+            }
+
+            public String getProperName() {
+                return properName;
+            }
+
+            public void setProperName(String properName) {
+                this.properName = properName;
+            }
+
+            public String getProperType() {
+                return properType;
+            }
+
+            public void setProperType(String properType) {
+                this.properType = properType;
+            }
+
+            public String getAnalyzer() {
+                return analyzer;
+            }
+
+            public void setAnalyzer(String analyzer) {
+                this.analyzer = analyzer;
+            }
+
+            public String getFormat() {
+                return format;
+            }
+
+            public void setFormat(String format) {
+                this.format = format;
+            }
+
+            public String getStore() {
+                return store;
+            }
+
+            public void setStore(String store) {
+                this.store = store;
+            }
         }
     }
 }
